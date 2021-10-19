1409d9b38ec3cee6c8644835dc263186b53f054d
Lonelypheonix <55979659+Lonelypheonix@users.noreply.github.com>
2021-10-19 10:42:36 +0530

    Create Lab sheet 1 P1.c

---
+++ Lab sheet 1/Lab sheet 1 P1.c
@@ -0,0 +1,14 @@
+#include<stdio.h> 
+//Pavan Kumar J 20201CCS0094
+int main()
+{
+int a, b;
+printf(" Enter the Value of a and b\n"); 
+scanf("%d%d",&a,&b);
+ if(a==b)
+ {
+printf("Inside the if statement\n"); 
+printf("a=%d \t b=%d are equal\n",a,b);
+ }
+printf("After the if statement control says am here\n");
+}
